a89c280b9d5305c629551236b06fee63594a1bd2
zXpect <zzzxpect2@gmail.com>
2024-10-15 09:53:33 -0500

    Subir proyecto

---
+++ src/main/java/com/jtl/demo_maikoll_felipe/DemoMaikollFelipeApplication.java
@@ -0,0 +1,13 @@
+package com.jtl.demo_maikoll_felipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMaikollFelipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMaikollFelipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jtl/demo_maikoll_felipe/DemoMaikollFelipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtl.demo_maikoll_felipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMaikollFelipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
